fbddb1c43a225717e57fc60294e37bf4ce98a3ac
Talha Ozleblebici <talha43gs@gmail.com>
2020-09-04 03:41:01 +0100

    Update QAcodingExerciseTest.java

--- src/test/java/QAcodingExerciseTest.java
+++ src/test/java/QAcodingExerciseTest.java
@@ -272,32 +272,4 @@ class QAcodingExerciseTest {
         // we can add timer and add limit for methods to done Performances test !!
     }
 
-
-
-
-
-
-
-
-
-    /**
-     * 1- I start my tests with the pre-code review. Basically, I review codes and methods. By doing syntax, execution and logic errors checks, I have done the basic tests.
-     *
-     * 2- First, I run the program with the happy test method by selecting small, simple size test data. In this way, I can do a basic function test.
-     *
-     * 3- Afterwards, I try to test the upper and lower limits of the method parameters by selecting the test data at the limit values.
-     * For example, I test all methods with a minimum (1) element parameter value. In addition, I do the parameter coverage tests with lower and upper limits.
-     * For example, for the "The total number of unique words across all lists" method, I do tests with test data that does not contain any unique data.
-     *
-     * 4- I do separate tests using repeating duplicate data and null data.
-     * For example, for the "words that appear in more than one list" method, I do tests using only the sample test data that is not in the other lists,
-     * repeating more than once in its own list.
-     *
-     * 5- I do negative status tests with sample data below the output values required by the methods.
-     * For example, for the "Top five frequent words in the all lists" method, I give data less than 5 words and test whether the method outputs according to the
-     * data without any errors.
-     * 6- Finally, I apply performance tests with extremely large test data.
-     * For example; I try to prepare sample test data as the max int value for all methods and run them.
-     */
-
 }
